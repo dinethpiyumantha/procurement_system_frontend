a8786399db3f71ab32fe8e7a41a76a96fe19edc4
Miru <madhaviimashi@gmail.com>
2021-10-04 09:07:07 +0530

    some changes done to NewRequisitions component

--- src/views/Staff/ManageRequisitions/NewRequisitions.vue
+++ src/views/Staff/ManageRequisitions/NewRequisitions.vue
@@ -1,15 +1,16 @@
 <template>
   <div class="container">
     <!--page header-->
-    <h3>New Requisitions</h3>
-    <span>(Requisitions with pending approval)</span>
+    <h2>New Requisitions</h2>
+    <h3 style="color: #888; line-height: 10px">{{ new Date() | moment("dddd, MMMM Do YYYY") }}</h3>
 
     <!--search and filter options-->
     <div class="row px-3 my-4">
         <!--search bar-->
         <a-input-search 
             placeholder="Search nurse by name" 
-            style="width: 400px; margin: 0 10px 0 0"
+            style="margin: 20px 0px;"
+            size="large"
             v-model="search" 
             @search="onSearch" 
         />
@@ -18,20 +19,15 @@
     <!--table structure-->
     <a-table
         :columns="columns"
-        :data-source="searchResult"
+        :data-source="requiData"
         :loading="loading"
         @change="handleTableChange"
         style="padding: 0px"
         :customRow = "customRow" 
+        :key="requisition_id"
     >
-        <!--Approve button-->
-        <!-- <a-button slot="action" type="primary" @click="approveRequisition"> </a-button>-->
-        <!--Reject button-->
-        <!-- <a-button slot="action" type="primary" @click="rejectRequisition"> </a-button> -->
 
-        <span>
-            <a-button slot="action" type="primary" size="small" @click="approveRequisition" >Approve</a-button>
-        </span>
+        <a-button class="approval_button" slot="action" type="primary" size="small"  >Approve / Reject</a-button>
         <!-- <span style="padding-left: 10px">
             <a-button slot="action2" type="danger" size="small" @click="rejectRequisition">Reject</a-button>
         </span> -->
@@ -39,15 +35,22 @@
 
         <!--when 1 row selected-> Model view including it's title-->
     <div>
-        <a-modal v-model="visible" :title="'Requisition - '+ model.name" on-ok="handleOk">
+        <a-modal v-model="visible" :title="'Requisition - '+ model.requisition_id" on-ok="handleOk">
             <!--model view content-->
             <div class="row"><div class="col-4"><b>requisition_id: </b></div><div class="col-8"><p>{{model.requisition_id}}</p></div></div>
             <div class="row"><div class="col-4"><b>good_type: </b></div><div class="col-8"><p>{{model.good_type}}</p></div></div>
+            <div class="row"><div class="col-4"><b>Quantity: </b></div><div class="col-8"><p>{{model.quantity}}</p></div></div>
             <div class="row"><div class="col-4"><b>estimated_budget: </b></div><div class="col-8"><p>{{model.estimated_budget}}</p></div></div>
-            <div class="row"><div class="col-4"><b>site_name: </b></div><div class="col-8"><p>{{model.site_name}}</p></div></div>
+            <div class="row"><div class="col-4"><b>Construction Site: </b></div><div class="col-8"><p>{{model.site_name}}</p></div></div>
        
             <!--model view footer-->  
             <template slot="footer">
+
+                <!-- Approve button -->
+                <a-button slot="action" type="primary" size="large" @click="approveRequisition">Approve </a-button>
+                <!-- Reject button -->
+                <a-button slot="action" style="margin-left: 10px;" type="danger" size="large" @click="rejectRequisition"> Reject</a-button>
+
                 <!--cancel button-->
                 <a-button key="back" @click="handleCancel">Cancel</a-button>
             </template>
@@ -79,18 +82,22 @@
             dataIndex: 'good_type',
         },
         {
-            title: 'Amount',
+            title: 'Quantity',
+            dataIndex: 'quantity',
+        },
+        {
+            title: 'Estimated_Budget',
             dataIndex: 'estimated_budget',
         },
         {
-            title: 'Site',
+            title: 'Construction_Site',
             dataIndex: 'site_name',
         },
         {
-            title: "",
+            title: "Actions",
             key: "id",
             fixed: "right",
-            width: 100,
+            width: 220,
             scopedSlots: { customRender: "action" },
         },
         // {
@@ -104,14 +111,18 @@
 
     export default {
         data() {
-            /**
-             *data attributes 
-             */
-            // getAllNurses_path : "http://127.0.0.1:8000/api/nurses/all", //this is the backend route to retrieve all nurse details
             
             return {
-                //return nurseData[] array which contains backend data
+                //return requiData[] array which contains backend data
                 requiData: [],
+                // requiData: {
+                //     requisition_id: '',
+                //     // date: '',
+                //     good_type: '',
+                //     quantity: '',
+                //     estimated_budget: '',
+                //     site_name: '',
+                // },
 
                 buttonWidth: 70,
                 pagination: { },
@@ -121,14 +132,15 @@
                 columns,
                 //return the values to v-model attributes that we've set in Search, Filter functions
                 search: '',
-                gender: '', 
-                nurse_type: '', //user selecting option of Filter dropdown will be assigned to this attribute
-                Shift: '',
+                good_type: '', 
+                quantity: '',
+                site_name: '',
                 //return model object(which contains all DB retrieved data as an obj)
                 model: { 
                     requisition_id: '',
                     // date: '',
                     good_type: '',
+                    quantity: '',
                     estimated_budget: '',
                     site_name: '',
                 } //**this model is useful when displaying DB retrieved data in our Modal windows */
@@ -139,9 +151,25 @@
              * get all nurse details from the Backend and store in the data array
              * using API request
              */
+            console.log("inside created method");
             axios.get("http://127.0.0.1:8001/api/staff/requisitions/get/pending").then((response) => {
-                    this.requiData = response.data.results;
+                    this.requiData = response.data.requisitions;
+                    console.log("response: ");
+                    console.log(response.data.requisitions);
+                    console.log("requiData: ");
                     console.log(this.requiData);
+                    // var status = response.status;
+                    // if(status == 200 || status == "200"){
+                    //     console.log("response: ");
+                    //     console.log(response.data);
+                    //     this.requiData = response.data;
+                    //     console.log("requiData: ");
+                    //     console.log(this.requiData);
+                    // }
+                    // else{
+                    //     alert("not 200");
+                    //     this.requiData = response.data
+                    // }                           
                 }
             );
         },
@@ -187,6 +215,65 @@
             onSearch(value){
                 console.log(value);
             },
+
+            //update approval status when approved
+            approveRequisition() {
+                this.$confirm({
+                    title: 'Are you sure to Approve the requisition - '+ this.model.requisition_id+ '?',
+                    content: "Click 'No' cancel approval",
+                    okText: 'Yes',
+                    okType: 'danger',
+                    cancelText: 'No',
+                    onOk: () => {
+                        this.$http.put("http://127.0.0.1:8001/api/staff/approve/" +this.model.id).then(
+                            function(response){
+                                //popup successful msg
+                                this.openNotificationSuccess('Requisition Approved successfully', 'Requisition - '+ this.model.requisition_id +' Accepted.');
+                                // setTimeout("location.reload(true);", 1000); //this will reload the page
+                                this.requiData.splice((this.requiData.findIndex((e) => e === this.model)), 1);
+                                console.log(response);                             
+                            }, (error) => {
+                                this.openNotificationUnsuccess('Error', 'Requisition'+ this.model.requisition_id +' requisition approval failed. Operation occured an error !');
+                                console.log(error);
+                                // alert("appproval unsuccesfful");
+                            }
+                        );
+                    },
+                    onCancel() {
+                    console.log('Cancel');
+                    },
+                });
+            },
+
+            //update approval status when rejected
+            rejectRequisition() {
+                this.$confirm({
+                    title: 'Are you sure to Reject the requisition - '+ this.model.requisition_id+ '?',
+                    content: "Click 'No' to cancel",
+                    okText: 'Yes',
+                    okType: 'danger',
+                    cancelText: 'No',
+                    onOk: () => {
+                        this.$http.put("http://127.0.0.1:8001/api/staff/reject/" +this.model.id).then(
+                            function(response){
+                                //popup successful msg
+                                this.openNotificationSuccess('Requisition Rejected successfully', 'Reuisition - '+ this.model.requisition_id +' rejected.');
+                                // setTimeout("location.reload(true);", 1000); //this will reload the page
+                                this.requiData.splice((this.requiData.findIndex((e) => e === this.model)), 1);
+                                console.log(response);                             
+                            }, (error) => {
+                                this.openNotificationUnsuccess('Error', 'Requisition'+ this.model.requisition_id +' Requisition reject Failed! . Operation occured an error !');
+                                console.log(error);
+                                // alert("rejecting was unsuccesfful");
+                            }
+                        );
+                    },
+                    onCancel() {
+                    console.log('Cancel');
+                    },
+                });
+            },
+
             //openNotificationSuccess() //for Delete function
             openNotificationSuccess(message, description) {
                 /**
@@ -218,42 +305,13 @@
                     },
                 });
             },
-
-            //showConfirm() method to confirm deletion of a row
-            showDeleteConfirm() {
-                this.$confirm({
-                    title: 'Are you sure to delete nurse - '+ this.model.nurse_no+'\n'+'('+ this.model.name+ ')'+ '?',
-                    content: "Click 'No' if you don't really want to delete this nurse",
-                    okText: 'Yes',
-                    okType: 'danger',
-                    cancelText: 'No',
-                    onOk: () => {
-                        this.$http.delete("http://127.0.0.1:8001/api/nurses/delete/" +this.model.id).then(
-                            function(response){
-                                //popup successful msg
-                                this.openNotificationSuccess('Successfully Deleted', 'Nurse - '+ this.model.nurse_no +' deleted.');
-                                // setTimeout("location.reload(true);", 1000); //this will reload the page
-                                this.requiData.splice((this.requiData.findIndex((e) => e === this.model)), 1);
-                                console.log(response);                             
-                            }, (error) => {
-                                this.openNotificationUnsuccess('Error', 'Nurse'+ this.model.nurse_no +' record cannot delete. Operation occured an error !');
-                                console.log(error);
-                                alert("delete unsuccesfful");
-                            }
-                        );
-                    },
-                    onCancel() {
-                    console.log('Cancel');
-                    },
-                });
-            },
             
         },
         computed: {
                 searchResult: function(){
                     return this.requiData.filter((item) => {
                         //dropdown selcted value or the searched value will be matched with the respective data in DB table
-                        return (item.gender.toLowerCase().match(this.gender.toLowerCase())) &&(item.Shift.toLowerCase().match(this.Shift.toLowerCase())) && (item.nurse_type.toLowerCase().match(this.nurse_type.toLowerCase())) && (item.name.toLowerCase().match(this.search.toLowerCase()));
+                        return (item.requisition_id.toLowerCase().match(this.requisition_id.toLowerCase())) &&(item.good_type.toLowerCase().match(this.good_type.toLowerCase())) && (item.estimated_budget.toLowerCase().match(this.estimated_budget.toLowerCase())) && (item.site_name.toLowerCase().match(this.site_name.toLowerCase()));
                     });
                 }
             }
@@ -274,5 +332,16 @@
     .container {
     background: #fff;
     padding: 20px;
-}
+    }
+
+    .approval_button{
+        background-color: white; 
+        color: black; 
+        border: 2px solid #f44336;
+    }
+
+    .approval_button:hover {
+    background-color: #f44336;
+    color: white;
+    }
 </style>
